2adc09e409edfd8d8086628e6d655df4dc4fac80
Fatima Siddique <fatimasidq@gmail.com>
2022-01-11 17:44:37 -0500

    updated seeds

--- db/seeds/01_seeds.sql
+++ db/seeds/01_seeds.sql
@@ -1,14 +1,18 @@
-
-
 DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS customers CASCADE;
-DROP TABLE IF EXISTS categories CASCADE;
-DROP TABLE IF EXISTS foods CASCADE;
-DROP TABLE IF EXISTS orders CASCADE;
-DROP TABLE IF EXISTS order_details CASCADE;
-DROP TABLE IF EXISTS orders CASCADE;
-DROP TABLE IF EXISTS widgets CASCADE;
 
+DROP TABLE IF EXISTS customers CASCADE;
+
+DROP TABLE IF EXISTS categories CASCADE;
+
+DROP TABLE IF EXISTS foods CASCADE;
+
+DROP TABLE IF EXISTS orders CASCADE;
+
+DROP TABLE IF EXISTS order_details CASCADE;
+
+DROP TABLE IF EXISTS orders CASCADE;
+
+DROP TABLE IF EXISTS widgets CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
@@ -16,10 +20,10 @@ CREATE TABLE users (
   phone VARCHAR(100) NOT NULL,
   email VARCHAR(100) NOT NULL,
   password VARCHAR(100) NOT NULL,
-  is_customer BOOLEAN NOT NULL,    -- optional
+  is_customer BOOLEAN NOT NULL,
+  -- optional
   user_since TIMESTAMP NOT NULL DEFAULT Now()
 );
-
 
 CREATE TABLE customers (
   id SERIAL PRIMARY KEY,
@@ -29,6 +33,7 @@ CREATE TABLE customers (
 );
 
 DROP TABLE IF EXISTS categories CASCADE;
+
 CREATE TABLE categories (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(100) UNIQUE NOT NULL,
@@ -41,12 +46,11 @@ CREATE TABLE foods (
   name VARCHAR(255) NOT NULL,
   description TEXT,
   url_image VARCHAR(255) NOT NULL,
-  price INTEGER  NOT NULL DEFAULT 0,
+  price INTEGER NOT NULL DEFAULT 0,
   category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
   estimated_time TIME NOT NULL,
   is_active BOOLEAN DEFAULT true
 );
-
 
 CREATE TABLE orders (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -61,7 +65,6 @@ CREATE TABLE orders (
   special_instructions TEXT
 );
 
-
 CREATE TABLE order_details (
   id SERIAL PRIMARY KEY NOT NULL,
   order_id INTEGER NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
@@ -70,56 +73,191 @@ CREATE TABLE order_details (
   total_amount FLOAT NOT NULL
 );
 
-
 -- Drop and recreate Widgets table (Example)
-
 CREATE TABLE widgets (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id),
   name VARCHAR(255) NOT NULL
 );
 
-
-
 -- Users table seeds here
-INSERT INTO users (name, phone, email, password, is_customer)
-VALUES ('labber', '123-456-7890','l@l.com', 'labber', false),
-('Fatima', '519-123-4567','f@f.com', '123', true),
-('Osman', '213-123-7890','o@o.com','123', true),
-('Stellaris', '416-1111-8888','s@m.com','123', true);
-
-INSERT INTO categories (name, description, is_active)
+INSERT INTO
+  users (name, phone, email, password, is_customer)
 VALUES
-('wraps-sandwiches', 'Hot and cold sandwiches and wraps', true),
-('salads', 'For healthier option', true),
-('pastries-desserts', 'Treats you will love to indulge', true),
-('soups', 'Comfort yourself with hot soups made with fresh ingredients', true),
-('beverages', 'Hot and cold beverages for everyones delight', true);
+  (
+    'labber',
+    '123-456-7890',
+    'l@l.com',
+    'labber',
+    false
+  ),
+  ('Fatima', '519-123-4567', 'f@f.com', '123', true),
+  ('Osman', '213-123-7890', 'o@o.com', '123', true),
+  (
+    'Stellaris',
+    '416-1111-8888',
+    's@m.com',
+    '123',
+    true
+  );
 
-
-INSERT INTO foods (name, description, url_image, price, category_id, preparation_time, is_active)
+INSERT INTO
+  categories (name, description, is_active)
 VALUES
-('Veggie Wrap', 'A colourful Veggie Wrap packed with fresh organic veggies of your choice wrapped in lettuce.', 'https://33mdrtv3gz5308bn82jcilk1-wpengine.netdna-ssl.com/wp-content/uploads/2018/07/goddess-wrap-768x768.jpg', 8, 1, 10, true),
-('Vegan breakfast burritos','Kidney beans, Sweet potatoes, avocados and beets wrapped in a tortilla','https://cdn2.veganosity.com/wp-content/uploads/2014/06/2014-06-21-001-2014-06-21-015.jpg', 8, 1, 10, true);
+  (
+    'Wraps and Sandwiches',
+    'Hot and cold sandwiches and wraps',
+    true
+  ),
+  ('Salads', 'For healthier option', true),
+  (
+    'Pastries and Desserts',
+    'Treats that you will love to indulge',
+    true
+  ),
+  (
+    'Beverages',
+    'Hot and cold beverages for everyone''s delight',
+    true
+  );
+
+INSERT INTO
+  foods (
+    name,
+    description,
+    url_image,
+    price,
+    category_id,
+    preparation_time,
+    is_active
+  )
+VALUES
+  (
+    'Veggie Wrap',
+    'A colourful Veggie Wrap packed with fresh organic veggies of your choice wrapped in lettuce.',
+    'assets/menu/veggie_wraps.png',
+    8,
+    1,
+    10,
+    true
+  ),
+  (
+    'Vegan breakfast burritos',
+    'Kidney beans, Sweet potatoes, avocados and beets wrapped in a tortilla',
+    'assets/menu/veggie_breakfast_burritos.png',
+    8,
+    1,
+    10,
+    true
+  );
 
 -- salad
-INSERT INTO foods (name, description, url_image, price, category_id, preparation_time, is_active)
+INSERT INTO
+  foods (
+    name,
+    description,
+    url_image,
+    price,
+    category_id,
+    preparation_time,
+    is_active
+  )
 VALUES
-('Mixed Veg Salad','Cold salad of mixed raw veggies in marinate.','http://www.blimpygirl.com/bgmain/wp-content/uploads/2013/07/IMG_4467.jpg', 8, 2, 15, true);
+  (
+    'Mixed Veg Salad',
+    'Corn, Cherry tomatoes, Chickpeas, Onions, Radishes, Coconut Bacon on a bed of Romaine lettuce topped with avocados',
+    'assets/menu/mixed_veg_salad.png',
+    8,
+    2,
+    10,
+    true
+  );
 
 -- pastries-desserts
-INSERT INTO foods (name, description, url_image, price, category_id, preparation_time, is_active)
+INSERT INTO
+  foods (
+    name,
+    description,
+    url_image,
+    price,
+    category_id,
+    preparation_time,
+    is_active
+  )
 VALUES
-('Buttery French Croissants','Light fluffy French Croissants baked fresh every few hours.','https://lh3.googleusercontent.com/proxy/jHEAqqUv0Vd0M_Elb1emTD4pKcVfH2UTNdV2lFxMtHDBqFFoDRoVAow_v5BKsl-uqQLq0Nmnli0fiyUClqprpN7MtXzOzr35aNMzYClP_O07td1XbtLNuG-vy2U6HmBLcDvz=w1200-h630-p-k-no-nu', 3, 3, 30, true),
-('Banana Blueberry Muffins','Light fluffy French Croissants baked fresh every few hours.','https://theprettybee.com/wp-content/uploads/2014/06/Moist-and-delicious-blueberry-muffins-made-with-almond-meal-and-spelt-flour.-.jpg', 2, 3, 30, true),
-('Carrot Muffins','Toddlers love these little muffins. These are definitely savoury muffins, but because cooked carrot is quite sweet tasting, and the added ingredient of orange juice is sweet, they are sweeter than you would imagine.','https://toddlerandme.co.uk/wp-content/uploads/2015/10/Carrot-Muffins-11.jpg', 1, 3, 30, true),
-('Decadent Chocolate Chunk Vegan Cookies',' Have chewy, chocolaty, chunky cookies anytime you want','https://www.fodmapeveryday.com/wp-content/uploads/2019/12/vertical-image-of-low-FODMAP-vegan-chocolate-chunks-cookies-one-of-them-sprinkled-with-salt.jpg', 2, 3, 30, true);
-
+  (
+    'Buttery French Croissants',
+    'Light fluffy French Croissants baked fresh every few hours.',
+    'assets/menu/buttery_french_croissants.png',
+    3,
+    3,
+    30,
+    true
+  ),
+  (
+    'Blueberry Muffins',
+    'Freshly picked blueberries baked into a delious soft muffin.',
+    'assets/menu/blueberry_muffins.png',
+    2,
+    3,
+    30,
+    true
+  ),
+  (
+    'Carrot Muffins',
+    'Carrot Muffins with Sweet cheese cake filling',
+    'assets/menu/carrot_muffins.png',
+    1,
+    3,
+    30,
+    true
+  ),
+  (
+    'Chewy Chocolate Chunk Cookies',
+    'Decadent Chocolate Chunk Vegan Cookies',
+    'assets/menu/chewy_chocolate_chunk_cookies.png',
+    2,
+    3,
+    30,
+    true
+  );
 
 -- beverage
-INSERT INTO foods (name, description, url_image, price, category_id, preparation_time, is_active)
+INSERT INTO
+  foods (
+    name,
+    description,
+    url_image,
+    price,
+    category_id,
+    preparation_time,
+    is_active
+  )
 VALUES
-
-('Coffee','Enjoy one of our light, medium, dark, decaffeinated, or flavored brews of the day, brewed from only the top 1% of Arabica beans in the world.','https://brewminate.com/wp-content/uploads/2016/06/CoffeeCan01.jpg', 2, 5, 5, true),
-('Jasmine Pearls Green Tea','Aromatic organic jasmine blossoms are used for scenting the most tender organic green tea leaves and buds then hand-rolled into small pearls. Exquisite in flavor and aroma, and reputed to increase circulation, ease tension and reduce cholesterol.','https://pngimg.com/uploads/tea/tea_PNG16893.png', 2, 5, 3, true),
-('Bubble Tea','A creamy, sweet, bubbly delight! Full of those chewy, satisfying, delightful little mouthfuls with plenty of sugar and either milk or coconut milk.','https://guestlist.net/uploads/articles/9/504_medium.jpg', 6, 5, 7, true);
+  (
+    'Signature Brewed Coffee',
+    'Enjoy one of our light, medium, dark, decaffeinated, or flavored brews of the day, brewed from only the top 1% of Arabica beans in the world.',
+    'assets/menu/coffee.png',
+    2,
+    4,
+    5,
+    true
+  ),
+  (
+    'Green Tea',
+    '',
+    'assets/menu/green_tea.png',
+    2,
+    4,
+    3,
+    true
+  ),
+  (
+    'Bubble Tea',
+    'A creamy, sweet, bubbly delight! Full of those chewy, satisfying, delightful little mouthfuls with plenty of sugar and either milk or coconut milk.',
+    'https://guestlist.net/uploads/articles/9/504_medium.jpg',
+    6,
+    4,
+    7,
+    true
+  );
--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,6 +1,20 @@
 -- Users table seeds here
-INSERT INTO users (name, phone, email, password, is_customer)
-VALUES ('labber', '123-456-7890','l@l.com', 'labber', false),
-('Fatima', '519-123-4567','f@f.com', '123', true),
-('Osman', '213-123-7890','o@o.com','123', true),
-('Stellaris', '416-1111-8888','s@m.com','123', true);
+INSERT INTO
+  users (name, phone, email, password, is_customer)
+VALUES
+  (
+    'labber',
+    '123-456-7890',
+    'l@l.com',
+    'labber',
+    false
+  ),
+  ('Fatima', '519-123-4567', 'f@f.com', '123', true),
+  ('Osman', '213-123-7890', 'o@o.com', '123', true),
+  (
+    'Stellaris',
+    '416-1111-8888',
+    's@m.com',
+    '123',
+    true
+  );
