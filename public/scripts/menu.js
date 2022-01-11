8db1db367e268921e07470b28c479898858458a6
Fatima Siddique <fatimasidq@gmail.com>
2022-01-11 17:54:54 -0500

    updated menu

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -1,19 +1,19 @@
 const menu = $("#menu");
 
 for (let category of Object.values(categories)) {
-  menu.append(`<h1>${category.name} - <span>${category.description}</span></h1>`)
+  menu.append(`<h1 class="category-name">${category.name}<br><span class="category-description">${category.description}</span></h1>`)
   for (let food of category.foods) {
     menu.append(`
     <div class="item">
       <div class="image">
         <div class="hover">  
-          <h2>${food.price}</h2>
+          <h2>$${food.price}</h2>
           ${id ? `<button id="${food.id}">Add to cart</button>` : ""}
         </div>
         <img src="${food.url_image}" alt="${food.name}">
       </div>
       <div class="info">
-        <h3><${food.name}></h5>
+        <h3>${food.name}</h3>
         <p class="description">${food.description}</p>
       </div>
     </div>`)
