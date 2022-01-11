75ea733e3ed3bb94bbeee81702c017a09ed261bd
Fatima Siddique <fatimasidq@gmail.com>
2022-01-11 12:26:15 -0500

    fixed merge conflict x2

--- db/schema/01_schema.sql
+++ db/schema/01_schema.sql
@@ -1,20 +1,11 @@
-<<<<<<< HEAD
--- DROP DATABASE [IF EXISTS] midterm;
-
--- CREATE DATABASE midterm
-=======
 -- DROP DATABASE [IF EXISTS] template1;
 
 -- CREATE DATABASE template1
->>>>>>> 940ca850c65ff644b0d05e9c749d6567bbd7eec0
 --     WITH
 --     OWNER = labber
 --     ENCODING = 'UTF8'
 --     CONNECTION LIMIT = -1;
 
-<<<<<<< HEAD
--- \c midterm;
-=======
 -- \c template1;
 
 DROP DATABASE [IF EXISTS] food;
@@ -26,7 +17,6 @@ CREATE DATABASE food
     CONNECTION LIMIT = -1;
 
 \c food;
->>>>>>> 940ca850c65ff644b0d05e9c749d6567bbd7eec0
 
 
 DROP TABLE IF EXISTS users CASCADE;
@@ -57,24 +47,13 @@ CREATE TABLE customers (
   phone VARCHAR(20) NOT NULL
 );
 
-<<<<<<< HEAD
-DROP TABLE IF EXISTS categories CASCADE;
-CREATE TABLE categories (
-  id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(100) UNIQUE NOT NULL,
-=======
 CREATE TABLE categories (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(100) NOT NULL,
->>>>>>> 940ca850c65ff644b0d05e9c749d6567bbd7eec0
   description TEXT,
   is_active BOOLEAN DEFAULT true
 );
 
-<<<<<<< HEAD
-
-=======
->>>>>>> 940ca850c65ff644b0d05e9c749d6567bbd7eec0
 CREATE TABLE foods (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
@@ -82,11 +61,7 @@ CREATE TABLE foods (
   url_image VARCHAR(255) NOT NULL,
   price INTEGER  NOT NULL DEFAULT 0,
   category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
-<<<<<<< HEAD
-  preparation_time INTEGER NOT NULL,
-=======
   estimated_time TIME NOT NULL,
->>>>>>> 940ca850c65ff644b0d05e9c749d6567bbd7eec0
   is_active BOOLEAN DEFAULT true
 );
 
@@ -104,28 +79,13 @@ CREATE TABLE orders (
   special_instructions TEXT
 );
 
-<<<<<<< HEAD
--- order_details_id – primary key of the table. It is set to auto_increment (the database will automatically give this column a value starting from 1).
--- order_id – foreign key that links to the order table.
--- menu_id – foreign key that links to the menu table.
--- amount – amount of the food or menu.
--- no_of_serving – refers to the number of orders.
--- total_amount – total amount is equal to amount multiply by the number of serving.
-
-=======
->>>>>>> 940ca850c65ff644b0d05e9c749d6567bbd7eec0
 
 CREATE TABLE order_details (
   id SERIAL PRIMARY KEY NOT NULL,
   order_id INTEGER NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
   food_id INTEGER NOT NULL REFERENCES foods(id) ON DELETE CASCADE,
-<<<<<<< HEAD
-  no_of_serving INTEGER NOT NULL,
-  total_amount FLOAT NOT NULL
-=======
   no_of_serving int(4) NOT NULL,
   total_amount float NOT NULL,
->>>>>>> 940ca850c65ff644b0d05e9c749d6567bbd7eec0
 );
 
 
@@ -136,7 +96,3 @@ CREATE TABLE widgets (
   user_id INTEGER REFERENCES users(id),
   name VARCHAR(255) NOT NULL
 );
-<<<<<<< HEAD
-=======
-
->>>>>>> 940ca850c65ff644b0d05e9c749d6567bbd7eec0
