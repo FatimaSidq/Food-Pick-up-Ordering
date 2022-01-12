dc8486a6754898e7bf5e3193dd1ff3cd527e5754
Fatima Siddique <fatimasidq@gmail.com>
2022-01-12 12:29:16 -0500

    created cart route

--- db/schema/01_schema.sql
+++ db/schema/01_schema.sql
@@ -1,33 +1,21 @@
--- DROP DATABASE [IF EXISTS] template1;
--- CREATE DATABASE template1
+-- DROP DATABASE [IF EXISTS] midterm;
+
+-- CREATE DATABASE midterm
 --     WITH
 --     OWNER = labber
 --     ENCODING = 'UTF8'
 --     CONNECTION LIMIT = -1;
--- \c template1;
-DROP DATABASE [IF EXISTS] food;
 
-CREATE DATABASE food WITH OWNER = labber ENCODING = 'UTF8' CONNECTION
-LIMIT
-  = -1;
+-- \c midterm;
 
-\ c food;
 
 DROP TABLE IF EXISTS users CASCADE;
-
 DROP TABLE IF EXISTS customers CASCADE;
-
 DROP TABLE IF EXISTS categories CASCADE;
-
 DROP TABLE IF EXISTS foods CASCADE;
-
 DROP TABLE IF EXISTS orders CASCADE;
-
 DROP TABLE IF EXISTS order_details CASCADE;
-
 DROP TABLE IF EXISTS orders CASCADE;
-
-DROP TABLE IF EXISTS widgets CASCADE;
 
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
@@ -36,20 +24,21 @@ CREATE TABLE users (
   email VARCHAR(100) NOT NULL,
   password VARCHAR(100) NOT NULL,
   is_customer BOOLEAN NOT NULL,
-  -- optional
   user_since TIMESTAMP NOT NULL DEFAULT Now()
 );
 
+
 CREATE TABLE customers (
   id SERIAL PRIMARY KEY,
-  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  name VARCHAR(255) NOT NULL,
-  phone VARCHAR(20) NOT NULL
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+  -- name VARCHAR(255) NOT NULL,
+  -- phone VARCHAR(20) NOT NULL
 );
 
+DROP TABLE IF EXISTS categories CASCADE;
 CREATE TABLE categories (
   id SERIAL PRIMARY KEY NOT NULL,
-  name VARCHAR(100) NOT NULL,
+  name VARCHAR(100) UNIQUE NOT NULL,
   description TEXT,
   is_active BOOLEAN DEFAULT true
 );
@@ -59,9 +48,9 @@ CREATE TABLE foods (
   name VARCHAR(255) NOT NULL,
   description TEXT,
   url_image VARCHAR(255) NOT NULL,
-  price INTEGER NOT NULL DEFAULT 0,
+  price INTEGER  NOT NULL DEFAULT 0,
   category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
-  estimated_time TIME NOT NULL,
+  preparation_time INTEGER NOT NULL,
   is_active BOOLEAN DEFAULT true
 );
 
@@ -69,24 +58,31 @@ CREATE TABLE orders (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
   amount INTEGER NOT NULL,
-  when_ordered TIMESTAMP DEFAULT now(),
-  when_completed TIMESTAMP DEFAULT now(),
-  when_pickedUp TIMESTAMP DEFAULT now(),
+  when_ordered TIMESTAMP NOT NULL DEFAULT now(),
+  when_completed TIMESTAMP,
+  sms_customer BOOLEAN DEFAULT false,
   is_paid BOOLEAN DEFAULT false,
-  is_picked_up BOOLEAN DEFAULT false,
-  is_ready BOOLEAN DEFAULT false,
+  when_pickedUp TIMESTAMP,
   special_instructions TEXT
 );
+
+
+-- order details stores the detailed food items the customer orders
+-- no_of_serving – refers to the number of orders.
+-- total_amount – total amount is equal to amount multiply by the number of serving.
+
+DROP TABLE IF EXISTS order_details CASCADE;
 
 CREATE TABLE order_details (
   id SERIAL PRIMARY KEY NOT NULL,
   order_id INTEGER NOT NULL REFERENCES orders(id) ON DELETE CASCADE,
   food_id INTEGER NOT NULL REFERENCES foods(id) ON DELETE CASCADE,
-  no_of_serving int(4) NOT NULL,
-  total_amount float NOT NULL,
+  no_of_serving INTEGER NOT NULL,
+  total_amount FLOAT NOT NULL -- no_of_serving * foods.price of the foods.id
 );
 
 -- Drop and recreate Widgets table (Example)
+
 CREATE TABLE widgets (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id),
