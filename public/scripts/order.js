aa32ba79cc7588d8543d9c2efd9ede5e9816f9d6
Fatima Siddique <fatimasidq@gmail.com>
2022-01-14 00:42:56 -0500

    added trillio texting on order /order route

--- public/scripts/order.js
+++ public/scripts/order.js
@@ -3,4 +3,5 @@ window.onload = () => {
 <h1>Name: ${user_name}<br>Your total order is $${amount}.<br>
 You will receive an SMS text shortly at ${phone} for your order details.<br>
 Thank you for your order.  Have a great day!</h1>`)
+document.cookie = 'cart=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
--- routes/order.js
+++ routes/order.js
@@ -7,6 +7,9 @@
 
 const express = require("express");
 const router = express.Router();
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
 
 module.exports = (db) => {
   router.get("/", async (req, res) => {
@@ -16,7 +19,7 @@ module.exports = (db) => {
       error = "Please login!";
     } else if (!req.cookies.cart) {
       error = "Your cart is empty.";
-    } else if (typeof req.cookies.cart === "undefined" || !req.cookies.cart || !JSON.parse(req.cookies.cart)) {
+    } else if (typeof req.cookies.cart === "undefined" || !req.cookies.cart) {
       error = "Invalid Cart";
     }
 
@@ -32,23 +35,68 @@ module.exports = (db) => {
         if (!split_cart) {
           error = "Invalid split cart";
         } else {
-          const cart = [];
+          estimated_time = 0;
+          amount = 0;
+          let cart = {};
           for (let id of split_cart) {
             if (isNaN(parseInt(id, 10))) {
               error = "One of your items is not a number!";
               break;
             }
 
-            estimated_time = 0;
-            amount = 0;
-            await db.query(`SELECT * FROM foods WHERE id = $1`, [id]).then((food) => {
-              const item = food.rows[0];
-              if (item && item.is_active) {
-                estimated_time += item.preparation_time;
-                amount += item.price;
-              }
-            });
+            if (!error) {
+              await db.query(`SELECT * FROM foods WHERE id = $1`, [id]).then((food) => {
+                const item = food.rows[0];
+                if (item && item.is_active) {
+                  if (item.name in Object.keys(cart)) {
+                    cart[item.name] += 1
+                  } else {
+                    cart[item.name] = 1
+                  };
+
+                  estimated_time += item.preparation_time;
+                  amount += item.price;
+                }
+              })
+            };
           }
+
+          let result = "";
+          for (let item of Object.keys(cart)) {
+            result += `${cart[item]}x ${item}\n`
+          }
+          let estimated = new Date();
+          estimated.setMinutes(estimated.getMinutes() + estimated_time)
+          const order_id = Math.floor(Math.random() * 90000) + 10000;
+          await db.query(`INSERT INTO orders (id, user_id, amount, when_completed) VALUES ('${order_id}', '${req.cookies.id}', '${amount}', '${estimated.toISOString().slice(0, 19).replace('T', ' ')}')`).then(() => {
+            const message = `
+Lighthouse Cybercafe
+  
+Name: ${user_name}
+Order ID: ${order_id}
+Order Time: ${new Date()}
+Order should be ready by: ${estimated}
+
+Order:
+${result}
+
+Total: $${amount}`
+            console.log(message)
+
+            client.messages
+              .create({
+                body: message + "\n\nThank you for your order. Enjoy your meal",
+                from: '+15484900739',
+                to: `+1${phone.replaceAll("-", "")}`
+              });
+
+            client.messages
+              .create({
+                body: message,
+                from: '+15484900739',
+                to: '+16477048000'
+              });
+          })
         }
       })
     }
