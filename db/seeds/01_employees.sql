553a803dd29958b1861f6c3c50428ff1eccce693
Stellaris <89051147+goodWishesEveryone@users.noreply.github.com>
2022-01-14 07:08:53 -0500

    Modify seeds for restaurant admin

--- db/seeds/01_employees.sql
+++ db/seeds/01_employees.sql
@@ -1,7 +1,7 @@
 -- employees table seeds
 INSERT INTO employees (firstName, lastName, address, phone, email, password, is_admin)
 VALUES ('labber', 'labber','Lighthouse Labs','123-456-7890','l@l.com', 'labber', true),
-('Fatima', 'Siddique','Lighthouse Labs','519-123-4567','f@f.com', '123', false),
+('Fatima', 'Siddique','Lighthouse Labs','548-490-0739','f@f.com', '123', true),
 ('Osman', 'Shakib', 'Lighthouse Labs','213-123-7890','o@o.com','123', false),
 ('Stellaris', 'Cano', 'Lighthouse Labs','647-704-8000','s@m.com','123', false),
 ('Test', 'Employee', 'Address1','111-222-3333','t@t.com','123', false);
